a269c6f1ce4753ac318c8978a3b0e8f9b2588f05
OrestPolusyn <opect.pol@i.ua>
2021-03-03 17:30:30 +0200

    fix

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   $(window).scroll(function () {
     $(".slider-item").each(function (i) {
-      let bottom_of_object = $(this).offset().top + $(this).outerHeight() * 2.5;
+      let bottom_of_object = $(this).offset().top + $(this).outerHeight() * 2;
       let bottom_of_window = $(window).scrollTop() + $(window).height();
 
       if (bottom_of_window > bottom_of_object) {
